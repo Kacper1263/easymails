e4a7d5554d696c852f4ba5f74e0f74aed60f1f06
Kacper Marcinkiewicz <marcinkiewicz.kacper@gmail.com>
2019-11-04 09:14:53 +0100

    Option to create mailer in custom service

--- README.md
+++ README.md
@@ -15,3 +15,15 @@ var mailer = new EasyMail('gmail', 'myMail@gmail.com', 'myPassword')
 //Send email
 mailer.sendMail('myFriend@mail.com', 'New mail', 'This is my new mail')
 ```
+<br />
+
+Or if you want to create mailer in custom service<br />
+```js
+//Require easymails
+var EasyMail = require('easymails')
+
+//Create mailer in custom service
+var mailer = new EasyMail('custom', 'myMail@gmail.com', 'myPassword', "smtp.mysmtp.com", 123)
+//Send email
+mailer.sendMail('myFriend@mail.com', 'New mail', 'This is my new mail')
+```
--- index.js
+++ index.js
@@ -6,22 +6,47 @@ const nodemailer = require('nodemailer')
 class EasyMail{
     /**
      * Send emails faster. Based on nodemailer
-     * @param {String} service E.g. "gmail"
+     * @param {String} service E.g. "gmail". If you want to provide custom host and port, set it to "custom"
      * @param {String} user Login to service
      * @param {String} password Password to service
+     * @param {String} host OPTIONAL - SMTP host
+     * @param {Number} port OPTIONAL - SMTP port
      */
-    constructor(service, user, password){
+    constructor(service, user, password, host, port){
         this.service = service
         this.user = user
         this.password = password
+        this.host = host
+        this.port = port
 
-        this.transporter = nodemailer.createTransport({
-            service: this.service,
-            auth: {
-                user: this.user,
-                pass: this.password
-            }
-        })
+        try{
+            if(service.toLowerCase() == "custom"){
+                if(!host || !port) {
+                    console.error("Easymails error: Host and Port is required in custom service! You must create new Easymail with Host and Port when you want to use custom service!")
+                    process.exit()
+                } 
+
+                this.transporter = nodemailer.createTransport({
+                    host: host,
+                    port: port,
+                    auth: {
+                        user: this.user,
+                        pass: this.password
+                    }
+                })
+            }else{
+                this.transporter = nodemailer.createTransport({
+                    service: this.service,
+                    auth: {
+                        user: this.user,
+                        pass: this.password
+                    }
+                })
+            }  
+        }catch(e){
+            console.log("Error while creating transporter: " + e)
+        }
+              
     }
     /**
      * @returns If success - nothing, if error - error
@@ -30,21 +55,25 @@ class EasyMail{
      * @param {String} html Body of email (plain text or HTML)
      */
     sendMail(receiver, subject, html){
-        this.mailOptions = {
-            from: this.user, // sender address
-            to: receiver, // list of receivers
-            subject: subject, // Subject line
-            html: html
+        try{
+            this.mailOptions = {
+                from: this.user, // sender address
+                to: receiver, // list of receivers
+                subject: subject, // Subject line
+                html: html
+            }
+    
+            this.transporter.sendMail(this.mailOptions, function(err, info){
+                if(err){
+                    return err
+                }
+                else{
+                    return
+                }
+            })
+        }catch(e){
+            console.log("Error while sending email: " + e)
         }
-
-        this.transporter.sendMail(this.mailOptions, function(err, info){
-            if(err){
-                return err
-            }
-            else{
-                return
-            }
-        })
     }
 }
 
